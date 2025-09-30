6926ebaa727ae8477c60e030718270fadb3eaafd
JuniorOliveira2 <vanderlaniopires@gmail.com>
2025-09-29 22:51:45 -0300

    Add files via upload

---
+++ super_trunfo.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+int main(){
+
+    int populacaoo;
+    int pontoturisticoo;
+    float pibb;
+    float areaa;
+    char cidadee[50];
+    char estadoo[50];
+    char cartaa[50];
+    
+
+
+    printf("Digite o código da carta 01: \n");
+    scanf("%s", &cartaa);
+
+    printf("Digite a letra do estado: \n");
+    scanf("%s", &estadoo);
+
+    printf("Digite a cidade:\n");
+    scanf("%s", cidadee);
+
+    printf("Digite a população:\n");
+    scanf("%d", &populacaoo);
+
+    printf("Digite a quantidade de pontos turísticos:\n");
+    scanf("%d", &pontoturisticoo);
+
+    printf("Digite a área:\n");
+    scanf("%f", &areaa);
+
+    printf("Digite o PIB:\n");
+    scanf("%f", &pibb);
+
+
+    printf("Carta 01: %s \n", cartaa);
+    printf("letra estado: %s \n", estadoo);
+    printf("Cidade: %s \n", cidadee);
+    printf("Populaçao: %d \n", populacaoo);
+    printf("Área: %f \n", areaa);
+    printf("PIB: %f \n", pibb);
+    printf("Pontos turisticos: %d \n", pontoturisticoo);
+
+
+    int populacao;
+    int pontoturistico;
+    float pib;
+    float area;
+    char cidade[50];
+    char estado[50];
+    char carta[50];
+
+    printf("Digite o código da carta 02: \n");
+    scanf("%s", &carta);
+
+    printf("Digite a letra do estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite a cidade:\n");
+    scanf("%s", cidade);
+
+    printf("Digite a população:\n");
+    scanf("%d", &populacao);
+
+    printf("Digite a quantidade de ponto turistico:\n");
+    scanf("%d", &pontoturistico);
+
+    printf("Digite a área:\n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB:\n");
+    scanf("%f", &pib);
+
+
+    printf("Carta 02: %s \n", carta);
+    printf("letra estado: %s \n", estado);
+    printf("Cidade: %s \n", cidade);
+    printf("Populaçao: %d \n", populacao);
+    printf("Área: %f \n", area);
+    printf("PIB: %f \n", pib);
+    printf("Pontos turisticos: %d ", pontoturistico);
+
+
+    return 0;
+
+}
+
+
